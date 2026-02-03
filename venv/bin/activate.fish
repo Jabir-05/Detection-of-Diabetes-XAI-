b1f322ce7dd780bf6faf1da209ded4bde5fd6450
Jabir-05 <knock5jabir@gmail.com>
2026-02-03 16:19:27 +0530

    Final

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jabirimteyaz/Desktop/diabetes-xai/venv"
+set -gx VIRTUAL_ENV "/Users/jabirimteyaz/Desktop/Detection-of-Diabetes-XAI-/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
